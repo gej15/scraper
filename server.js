5dbb2230a46c9966a0f75cc9099ba7c8124df0fd
Grant Jackson <gej1515@gmail.com>
2019-12-19 15:49:49 -0600

    i dont like heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 // Require all models
-var db = require("./models")(app);
+var db = require("/models");
 
 var PORT = process.env.PORT || 3001;
 
